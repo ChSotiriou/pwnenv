ed150a6541e852f8189190cee31da286941f861c
Christos Soteriou <schris15@hotmail.com>
2020-10-07 09:59:45 +0100

    Added ability to run GUI apps with X11 server

--- run_scripts/run_arm.ps1
+++ run_scripts/run_arm.ps1
@@ -1,1 +1,1 @@
-docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it --rm --name pwnenvarm -v "$(get-location):/home/pwn/data".ToLower() pwnenvarm 
+docker run --env="DISPLAY=$(ipconfig | grep "Wireless LAN adapter Wi-Fi" -A 7 | grep -Po 'IPv4 Address[.\h]+: \K\S+'):0" --net=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it --rm --name pwnenvarm -v "$(get-location):/home/pwn/data".ToLower() pwnenvarm 
