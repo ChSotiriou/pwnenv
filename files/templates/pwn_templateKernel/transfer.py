a6ddd79aeebf55255c860ed260f499032e985d3d
eioake01 <ioakim39@gmail.com>
2022-12-25 23:16:22 +0200

    remove unnecessary brackets in pwn arm template

--- files/templates/pwn_templateKernel/transfer.py
+++ files/templates/pwn_templateKernel/transfer.py
@@ -7,25 +7,27 @@ import time
 import base64
 import os
 
+
 def run(cmd):
     sock.sendlineafter("$ ", cmd)
     sock.recvline()
 
+
 with open("./initramfs/xpl", "rb") as f:
     payload = bytes2str(base64.b64encode(f.read()))
 
-host = ''
+host = ""
 port = 0
 sock = Socket(host, port)
 
-run('cd /tmp')
+run("cd /tmp")
 
 logger.info("Uploading...")
 for i in range(0, len(payload), 512):
     print(f"Uploading... {i:x} / {len(payload):x}")
-    run('echo "{}" >> b64exp'.format(payload[i:i+512]))
-run('base64 -d b64exp > xpl')
-run('rm b64exp')
-run('chmod +x xpl')
+    run('echo "{}" >> b64exp'.format(payload[i : i + 512]))
+run("base64 -d b64exp > xpl")
+run("rm b64exp")
+run("chmod +x xpl")
 
 sock.interactive()
